29ffc421c6aabef8a7a974f01b7f887a304cab4b
fletcherdeal92 <fletcherdeal@gmail.com>
2022-07-06 20:15:22 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Fletcher Deal on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
